522e6ddec3459c8c492d360c7610e230c3a33c3d
Sahilgill24 <gursahilgill24@gmail.com>
2024-09-04 20:40:52 +0530

    analytics dashboard trial

---
+++ analytics/analytics.js
@@ -0,0 +1,10 @@
+const ethers = require('ethers');
+const axios = require('axios');
+
+const url = 'https://api.studio.thegraph.com/proxy/71118/ssv-network-holesky/version/latest'
+
+
+
+// ssv videos both --> restaking app + data analytics 
+// DKG ceremony UI 
+// tonight 
