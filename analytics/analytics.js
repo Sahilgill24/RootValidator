40344504bfd6e974bece1b4c7b7eebb898998222
Sahilgill24 <gursahilgill24@gmail.com>
2024-09-06 01:17:01 +0530

    testing

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -1,10 +1,31 @@
-const ethers = require('ethers');
 const axios = require('axios');
 
-const url = 'https://api.studio.thegraph.com/proxy/71118/ssv-network-holesky/version/latest'
+const url = 'https://api.studio.thegraph.com/proxy/71118/ssv-network-holesky/version/latest';
 
+async function getAccountData(account) {
+    const query = `
+query MyQuery {
+    account(id: "${account}") {
+        id
+        validatorCount
+        validators(first: 10, orderBy: id) {
+            id
+        }
+    }
+}`;
 
+    try {
+        const response = await axios.post(url, {
+            headers: { 'Content-Type': 'application/json' },
 
-// ssv videos both --> restaking app + data analytics 
-// DKG ceremony UI 
-// tonight 
+            query: query
+
+        });
+
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error fetching account data:', error);
+    }
+}
+
+getAccountData("0x00000000092c1d0569858c6c09b677540bee2d30");
---
+++ dkg_gui/ideation.md
@@ -0,0 +1,2 @@
+// how to run the dkg ceremony without using the docker container 
+// running it in the background 
