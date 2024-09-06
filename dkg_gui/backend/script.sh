37efef98bfbb1c5fcc84f49b18af9d201386412c
Sahilgill24 <gursahilgill24@gmail.com>
2024-09-07 03:30:33 +0530

    dkg ceremony architecture

---
+++ dkg_gui/backend/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# function for Linux (Ubuntu)
+
+install_docker_Linux() {
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce
+    sudo usermod -aG docker ${USER}
+    sudo systemctl enable docker
+    sudo systemctl start docker
+}
+
+install_docker_mac() {
+    echo "installing docker for mac"
+}
---
+++ dkg_gui/backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { exec } = require('child_process');
+const fs = require('fs');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+
+})
+
+
+function run_script() {
+    // for docker installation etc. 
+}
+
+function config() {
+    // will write the init.yaml file    
+}
