c07d27f233a5e6a264f53f0c552eb47c53e2b088
Sahilgill24 <gursahilgill24@gmail.com>
2024-09-08 01:21:45 +0530

    yaml initiation

--- dkg_gui/backend/server.js
+++ dkg_gui/backend/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const { exec } = require('child_process');
 const fs = require('fs');
+const yaml = require('js-yaml');
+const path = require('path');
 
 const app = express();
+app.use(express.json());
+
+
+const operatorfilepath = path.join(__dirname, 'config', 'operatorsinfo.json');
+const yamlFilePath = path.join(__dirname, 'config', 'init.yaml');
 
 app.get('/', (req, res) => {
-    exec('./testing.sh', (err, stdout, stderr) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(stdout);
-    });
+
 
 
 })
@@ -23,11 +24,20 @@ app.get('/create_config', (req, res) => {
 
 function run_script() {
     // for docker installation etc. 
-    
+    exec('./testing.sh', (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(stdout);
+    });
+
 }
 
-function config() {
-    // will write the init.yaml file    
+function config(config) {
+    // will write the init.yaml file 
+    const { validators, withdrawAddress, owner, nonce } = config
+
 }
 
-app.listen(3000, () => { console.log("working ")})
+app.listen(3000, () => { console.log("working ") })
